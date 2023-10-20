dcbe2e3d4fdb9d1187121a9131152d82af4fc2a8
admin <bohdan.chornyi@checkbox.ua>
2023-10-20 22:29:19 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,81 +7,66 @@ void main() {
         body: Container(
           decoration: BoxDecoration(color: Colors.white),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Redirect to Main page');
-                    },
-                    child: Text(
-                      'Main Page',
-                      textDirection: TextDirection.ltr,
-                    ),
+                child: ElevatedButton(
+                  onPressed: () {
+                    print('Redirect to Main page');
+                  },
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 50)),
+                  child: Text(
+                    'Main Page',
+                    textDirection: TextDirection.ltr,
                   ),
                 ),
               ),
               Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Redirect to Portfolio page');
-                    },
-                    child: Text(
-                      'Portfolio',
-                      textDirection: TextDirection.ltr,
-                    ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 50)),
+                  onPressed: () {
+                    print('Redirect to Portfolio page');
+                  },
+                  child: Text(
+                    'Portfolio',
+                    textDirection: TextDirection.ltr,
                   ),
                 ),
               ),
               Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Redirect to Order page');
-                    },
-                    child: Text(
-                      'Order',
-                      textDirection: TextDirection.ltr,
-                    ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 50)),
+                  onPressed: () {
+                    print('Redirect to Order page');
+                  },
+                  child: Text(
+                    'Order',
+                    textDirection: TextDirection.ltr,
                   ),
                 ),
               ),
               Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Redirect to Legal info page');
-                    },
-                    child: Text(
-                      'Legal info',
-                      textDirection: TextDirection.ltr,
-                    ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 50)),
+                  onPressed: () {
+                    print('Redirect to Legal info page');
+                  },
+                  child: Text(
+                    'Legal info',
+                    textDirection: TextDirection.ltr,
                   ),
                 ),
               ),
               Center(
-                child: SizedBox(
-                  width: 200,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      print('Redirect to Login page');
-                    },
-                    child: Text(
-                      'Log in',
-                      textDirection: TextDirection.ltr,
-                    ),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(fixedSize: Size(100, 50)),
+                  onPressed: () {
+                    print('Redirect to Login page');
+                  },
+                  child: Text(
+                    'Log in',
+                    textDirection: TextDirection.ltr,
                   ),
                 ),
               ),
